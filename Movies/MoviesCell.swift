453e98b58eec09e44661f6084f1cf7f557b4485f
bwong <bwong@sfs-wifi-dhcp-10-143-214-131.wifi.sfsu.edu>
2017-01-28 16:21:06 -0800

    first commit

---
+++ Movies/MoviesCell.swift
@@ -0,0 +1,27 @@
+//
+//  MoviesCell.swift
+//  Movies
+//
+//  Created by mwong on 1/23/17.
+//  Copyright © 2017 mwong. All rights reserved.
+//
+
+import UIKit
+
+class MoviesCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+
+}
